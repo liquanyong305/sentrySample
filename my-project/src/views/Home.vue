0c8cc78e275c770e3166d0346471942e520f1200
liquanyong305 <liquanyong@gmail.com>
2019-11-03 00:17:55 +0900

    test04

--- my-project/src/views/Home.vue
+++ my-project/src/views/Home.vue
@@ -15,7 +15,6 @@ export default {
     HelloWorld
   },
   created() {
-    console.log('Home error!');
     throw new Error('error occored');
   }
 
--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -1,4 +1,28 @@
 const SentryPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
+    pages: {
+        index: {
+          // entry for the page
+          entry: 'src/main.js',
+          // the source template
+          template: 'public/index.html',
+          // output as dist/index.html
+          filename: 'index.html',
+          // when using title option,
+          // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+          title: 'Index Page',
+          // chunks to include on this page, by default includes
+          // extracted common chunks and vendor chunks.
+          chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+    },
+    configureWebpack: {
+        plugins: [
+            new SentryPlugin({
+                release: 'test02',
+                include: './dist'
+            })
+        ]
+    }
 }
